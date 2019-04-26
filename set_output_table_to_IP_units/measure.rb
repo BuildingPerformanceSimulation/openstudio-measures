e37f41e68b70968d4934f87062052667fc293266
mdahlhausen <matthew.dahlhausen@gmail.com>
2019-04-26 15:55:49 -0600

    fix tests and formatting for set output table to IP units measure

--- set_output_table_to_IP_units/measure.rb
+++ set_output_table_to_IP_units/measure.rb
@@ -1,36 +1,42 @@
 class SetOutputTableToIPunits < OpenStudio::Ruleset::WorkspaceUserScript
 
   def name
-    return "Set Output Table To IP Units"
+    return 'Set Output Table To IP Units'
   end
 
+  # human readable description
   def description
-	  return "This measure changes the output table to be in IP units"
+    return 'This measure changes the output table to be in IP units.'
   end
-  
-  #define the arguments that the user will input
+
+  # human readable description of modeling approach
+  def modeler_description
+    return 'This EnergyPlus measure sets the OutputControl:Table:Style to InchPound.'
+  end
+
+  # define the arguments that the user will input
   def arguments(workspace)
     args = OpenStudio::Ruleset::OSArgumentVector.new
     return args
-  end #end the arguments method
+  end
 
-  #define what happens when the measure is run
+  # define what happens when the measure is run
   def run(workspace, runner, user_arguments)
     super(workspace, runner, user_arguments)
 
-    #if IP units requested add OutputControl:Table:Style object
+    # if IP units requested add OutputControl:Table:Style object
     table_style = workspace.getObjectsByType("OutputControl:Table:Style".to_IddObjectType)
-    
-    #even though there is just a single object, it is still in an array
+
+    # even though there is just a single object, it is still in an array
     if not table_style.empty?
-      #we can access the first object in the array using the table_style[0]
-      #use setString to change the field value to request IP units
+      # we can access the first object in the array using the table_style[0]
+      # use setString to change the field value to request IP units
       table_style_ip = table_style[0].setString(1,"InchPound")
     end
-    return true
-  end #end the run method
-end #end the measure
-	
 
-#this allows the measure to be use by the application
+    return true
+  end
+end
+
+# this allows the measure to be use by the application
 SetOutputTableToIPunits.new.registerWithApplication
--- set_output_table_to_IP_units/tests/SetOutputTableToIPUnits_Test.rb
+++ set_output_table_to_IP_units/tests/SetOutputTableToIPUnits_Test.rb
@@ -4,55 +4,59 @@ require 'minitest/autorun'
 require_relative '../measure.rb'
 require 'fileutils'
 
-class SetOutputTableToIPUnits_Test < MiniTest::Unit::TestCase
+class SetOutputTableToIPUnits_Test < MiniTest::Test
 
   def workspace_out_path(test_name)
     # always generate test output in specially named 'output' directory so result files are not made part of the measure
     return "#{File.dirname(__FILE__)}/output/#{test_name}.idf"
   end
-  
+
   def test_number_of_arguments_and_argument_names
     # this test ensures that the current test is matched to the measure inputs
-    
+
     # create an instance of the measure
     measure = SetOutputTableToIPunits.new
-    
+
     #load the example workspace
     workspace = OpenStudio::Workspace.new
-    
+
     # get arguments and test that they are what we are expecting
     arguments = measure.arguments(workspace)
-    assert_equal(0, arguments.size)   
+    assert_equal(0, arguments.size)
   end
 
   def test_run
     # this tests good input values
     test_name = "test_run"
-    
+
     # create an instance of the measure
     measure = SetOutputTableToIPunits.new
-    
+
     # create an instance of a runner
     runner = OpenStudio::Ruleset::OSRunner.new
-    
+
     # load the workspace
-    workspace = OpenStudio::Workspace::load(OpenStudio::Path.new("./example.idf")).get
-    
+    idf_file = File.dirname(__FILE__) + '/example.idf'
+    assert(File.exists?(idf_file))
+    workspace = OpenStudio::Workspace::load(OpenStudio::Path.new(idf_file))
+    assert((not workspace.empty?))
+    workspace = workspace.get
+
     # set argument values to good values
     arguments = measure.arguments(workspace)
     argument_map = OpenStudio::Ruleset::OSArgumentMap.new
-    
+
     # run the measure
     measure.run(workspace, runner, argument_map)
     result = runner.result
-    
+
     # show the output
     show_output(result)
-    
+
     # assert that it ran correctly
     assert_equal("Success", result.value.valueName)
     assert(result.warnings.size == 0)
-    
+
     #save the workspace for testing purposes
     if !File.exist?("#{File.dirname(__FILE__)}/output")
       FileUtils.mkdir_p("#{File.dirname(__FILE__)}/output")
@@ -60,5 +64,5 @@ class SetOutputTableToIPUnits_Test < MiniTest::Unit::TestCase
     output_file_path = workspace_out_path(test_name)
     workspace.save(output_file_path,true)
   end
-  
+
 end
