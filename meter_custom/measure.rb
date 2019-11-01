45abedb2e7a9f09f20f5a62324daf780e1350e20
mdahlhausen <matthew.dahlhausen@gmail.com>
2019-10-31 18:15:29 -0600

    update measure to use MeterCustom object in the OS API

--- meter_custom/measure.rb
+++ meter_custom/measure.rb
@@ -1,245 +1,140 @@
-# see the URL below for information on how to write OpenStudio measures
-# http://nrel.github.io/OpenStudio-user-documentation/measures/measure_writing_guide/
-
 require 'openstudio'
 require 'csv'
 
 # start the measure
-class MeterCustom < OpenStudio::Ruleset::WorkspaceUserScript
+class MeterCustom < OpenStudio::Measure::ModelMeasure
 
   # human readable name
   def name
-    return "Custom Meter"
+    return 'Meter Custom'
   end
 
   # human readable description
   def description
-    return "This measure creates a Custom Meter to combine output variables and meters from a user-defined .csv file.  This is helpful for combining energy use by floor, by space type, or for specific zones. You can easily create this grouping by generating the SpaceTypeReport, then filtering and copying the zones you want to group together. An example .csv file is included in the resources folder for this measure. The measure optionally outputs the custom meter."
+    return 'This measure creates a Custom Meter to combine output variables and meters from a user-defined .csv file. This is helpful for combining energy use by floor, by space type, or for specific zones. You can easily create this grouping by generating the SpaceTypeReport, then filtering and copying the zones you want to group together. An example .csv file is included in the resources folder for this measure. The measure optionally outputs the custom meter.  An example file is included in the tests directory of this measure.'
   end
-  
+
   # human readable description of modeling approach
   def modeler_description
-    return "This measure creates a Meter:Custom object based on a user-defined .csv file and optionally adds an Output:Meter for the custom meter to save the values to the .eio and .mtr files.  Common errors: (1) Using space names instead of thermal zone names as key variables, (2) not specifying the Zone variable, e.g. Lights Electric Energy vs. Zone Lights Electric Energy, (3) Requesting a variable or meter that is not there.  E.g., if a zone has no electric equipment, you cannot request a Zone Electric Equipment Electric Energy variable."
+    return 'This measure creates a Meter:Custom object based on a user-defined .csv file and optionally adds an Output:Meter for the custom meter to save the values to the .eio and .mtr files. Common errors include (1) using space names instead of thermal zone names as key variables, (2) not specifying the Zone variable, e.g. Lights Electric Energy vs. Zone Lights Electric Energy, (3) combining different fuel types on the same meter, and (4) Requesting a variable or meter that is not there, e.g., if a zone has no electric equipment, you cannot request a Zone Electric Equipment Electric Energy variable.'
   end
-  
-  # define the arguments that the user will input
-  def arguments(workspace)
-    args = OpenStudio::Ruleset::OSArgumentVector.new
 
-    #make string argument for meter name
-    custom_meter_name = OpenStudio::Ruleset::OSArgument::makeStringArgument("custom_meter_name", true)
-    custom_meter_name.setDisplayName("Name of Custom Meter:")
-    custom_meter_name.setDefaultValue("custom meter name")
+  # define the arguments that the user will input
+  def arguments
+    args = OpenStudio::Measure::OSArgumentVector.new
+
+    # make string argument for meter name
+    custom_meter_name = OpenStudio::Measure::OSArgument.makeStringArgument('custom_meter_name', true)
+    custom_meter_name.setDisplayName('Name of Custom Meter:')
+    custom_meter_name.setDefaultValue('custom_meter_name')
     args << custom_meter_name
 
-    #make choice argument for fuel type
+    # make choice argument for fuel type
     choices = OpenStudio::StringVector.new
-    choices << "Electricity"
-    choices << "NaturalGas"
-    choices << "PropaneGas"
-    choices << "FuelOil#1"
-    choices << "FuelOil21"
-    choices << "Coal"
-    choices << "Diesel"
-    choices << "Water"
-    choices << "Generic"
-    choices << "OtherFuel1"
-    choices << "OtherFuel2"
-    fuel_type = OpenStudio::Ruleset::OSArgument::makeChoiceArgument("fuel_type", choices, true)
-    fuel_type.setDisplayName("Fuel Type:")
-    fuel_type.setDefaultValue("Electricity")
+    choices << 'Electricity'
+    choices << 'NaturalGas'
+    choices << 'PropaneGas'
+    choices << 'FuelOil#1'
+    choices << 'FuelOil21'
+    choices << 'Coal'
+    choices << 'Diesel'
+    choices << 'Water'
+    choices << 'Generic'
+    choices << 'OtherFuel1'
+    choices << 'OtherFuel2'
+    fuel_type = OpenStudio::Measure::OSArgument::makeChoiceArgument('fuel_type', choices, true)
+    fuel_type.setDisplayName('Fuel Type:')
+    fuel_type.setDefaultValue('Electricity')
     args << fuel_type
 
-    #file path to csv detailing what is on the meter
-    file_path = OpenStudio::Ruleset::OSArgument.makeStringArgument("file_path", true)
-    file_path.setDisplayName("Enter the path to the file:")
-    file_path.setDescription("Example: 'C:\\MyProject\\custom_meter_assignment.csv'")
+    # file path to csv detailing what is on the meter
+    file_path = OpenStudio::Measure::OSArgument.makeStringArgument('file_path', true)
+    file_path.setDisplayName('Enter the path to the file:')
+    file_path.setDefaultValue("'C:\\MyProject\\custom_meter_assignment.csv'")
     args << file_path
-    
-    add_output_meter = OpenStudio::Ruleset::OSArgument.makeBoolArgument("add_output_meter",true)
-    add_output_meter.setDisplayName("Include associated Output:Meter object?")
+
+    add_output_meter = OpenStudio::Measure::OSArgument.makeBoolArgument('add_output_meter',true)
+    add_output_meter.setDisplayName('Include associated Output:Meter object?')
     add_output_meter.setDefaultValue(true)
     args << add_output_meter
-    
+
     reporting_frequency_chs = OpenStudio::StringVector.new
-    reporting_frequency_chs << "detailed"
-    reporting_frequency_chs << "timestep"
-    reporting_frequency_chs << "hourly"
-    reporting_frequency_chs << "daily"
-    reporting_frequency_chs << "monthly"
-    reporting_frequency = OpenStudio::Ruleset::OSArgument::makeChoiceArgument("reporting_frequency", reporting_frequency_chs, true)
-    reporting_frequency.setDisplayName("Select reporting frequency for Output:Meter object:")
-    reporting_frequency.setDefaultValue("hourly")
+    reporting_frequency_chs << 'detailed'
+    reporting_frequency_chs << 'timestep'
+    reporting_frequency_chs << 'hourly'
+    reporting_frequency_chs << 'daily'
+    reporting_frequency_chs << 'monthly'
+    reporting_frequency = OpenStudio::Measure::OSArgument::makeChoiceArgument('reporting_frequency', reporting_frequency_chs, true)
+    reporting_frequency.setDisplayName('Select reporting frequency for Output:Meter object:')
+    reporting_frequency.setDefaultValue('hourly')
     args << reporting_frequency
 
     return args
   end
-  
-  # check to see if we have an exact match for this object already
-  def check_for_object(runner, workspace, idf_object, idd_object_type)
-    workspace.getObjectsByType(idd_object_type).each do |object|
-      # all of these objects fields are data fields
-      if idf_object.dataFieldsEqual(object)
-        return true
-      end
-    end
-    return false
-  end
-  
-  # merge all summary reports that are not in the current workspace
-  def merge_output_table_summary_reports(current_object, new_object)
-  
-    current_fields = []
-    current_object.extensibleGroups.each do |current_extensible_group|
-      current_fields << current_extensible_group.getString(0).to_s
-    end
-        
-    fields_to_add = []
-    new_object.extensibleGroups.each do |new_extensible_group|
-      field = new_extensible_group.getString(0).to_s
-      if !current_fields.include?(field)
-        current_fields << field
-        fields_to_add << field
-      end
-    end
-    
-    if !fields_to_add.empty?
-      fields_to_add.each do |field|
-        values = OpenStudio::StringVector.new
-        values << field
-        current_object.pushExtensibleGroup(values)
-      end
-      return true
-    end
-    
-    return false
-  end
-  
-  # examines object and determines whether or not to add it to the workspace
-  def add_object(runner, workspace, idf_object)
-
-    num_added = 0
-    idd_object = idf_object.iddObject
-   
-    allowed_objects = []
-    allowed_objects << "Output:Surfaces:List"
-    allowed_objects << "Output:Surfaces:Drawing"
-    allowed_objects << "Output:Schedules"
-    allowed_objects << "Output:Constructions"
-    allowed_objects << "Output:Table:TimeBins"
-    allowed_objects << "Output:Table:Monthly"
-    allowed_objects << "Output:Variable"
-    allowed_objects << "Output:Meter"
-    allowed_objects << "Output:Meter:MeterFileOnly"
-    allowed_objects << "Output:Meter:Cumulative"
-    allowed_objects << "Output:Meter:Cumulative:MeterFileOnly"
-    allowed_objects << "Meter:Custom"
-    allowed_objects << "Meter:CustomDecrement"
-    
-    if allowed_objects.include?(idd_object.name)
-      if !check_for_object(runner, workspace, idf_object, idd_object.type)
-        runner.registerInfo("Adding idf object #{idf_object.to_s.strip}")
-        workspace.addObject(idf_object)
-        num_added += 1
-      else
-        runner.registerInfo("Workspace already includes #{idf_object.to_s.strip}")
-      end
-    end
-    
-    allowed_unique_objects = []
-    #allowed_unique_objects << "Output:EnergyManagementSystem" # TODO: have to merge
-    #allowed_unique_objects << "OutputControl:SurfaceColorScheme" # TODO: have to merge
-    allowed_unique_objects << "Output:Table:SummaryReports" # TODO: have to merge
-    # OutputControl:Table:Style # not allowed
-    # OutputControl:ReportingTolerances # not allowed
-    # Output:SQLite # not allowed
-   
-    if allowed_unique_objects.include?(idf_object.iddObject.name)
-      if idf_object.iddObject.name == "Output:Table:SummaryReports"
-        summary_reports = workspace.getObjectsByType(idf_object.iddObject.type)
-        if summary_reports.empty?
-          runner.registerInfo("Adding idf object #{idf_object.to_s.strip}")
-          workspace.addObject(idf_object)
-          num_added += 1
-        elsif merge_output_table_summary_reports(summary_reports[0], idf_object)
-          runner.registerInfo("Merged idf object #{idf_object.to_s.strip}")     
-        else
-          runner.registerInfo("Workspace already includes #{idf_object.to_s.strip}")
-        end
-      end
-    end
-    
-    return num_added
-  end
 
   # define what happens when the measure is run
-  def run(workspace, runner, user_arguments)
-    super(workspace, runner, user_arguments)
-    
+  def run(model, runner, user_arguments)
+    super(model, runner, user_arguments)
+
     # use the built-in error checking 
-    if not runner.validateUserArguments(arguments(workspace), user_arguments)
+    unless runner.validateUserArguments(arguments, user_arguments)
       return false
     end
-    
-    #assign the user inputs to variables
-    custom_meter_name = runner.getStringArgumentValue("custom_meter_name",user_arguments)
-    fuel_type = runner.getStringArgumentValue("fuel_type",user_arguments)
-    file_path = runner.getStringArgumentValue("file_path",user_arguments)
-    add_output_meter = runner.getBoolArgumentValue("add_output_meter",user_arguments)
-    reporting_frequency = runner.getStringArgumentValue("reporting_frequency",user_arguments)
-    
+
+    # assign the user inputs to variables
+    custom_meter_name = runner.getStringArgumentValue('custom_meter_name', user_arguments)
+    fuel_type = runner.getStringArgumentValue('fuel_type', user_arguments)
+    file_path = runner.getStringArgumentValue('file_path', user_arguments)
+    add_output_meter = runner.getBoolArgumentValue('add_output_meter', user_arguments)
+    reporting_frequency = runner.getStringArgumentValue('reporting_frequency', user_arguments)
+
     # check the file path for reasonableness
     if file_path.empty?
-      runner.registerError("Empty path was entered.")
+      runner.registerError('Empty path was entered.')
       return false
     end
-    
-    runner.registerWarning("Looking for file at file_path 1: #{file_path})")
-    # Strip out the potential leading and trailing quotes  
-    file_path.gsub!('"','')
-    
-    meter_definition = "Meter:Custom," + custom_meter_name + "," + fuel_type
-    runner.registerWarning("Looking for file at file_path 2: #{file_path})")
-    
-    
+
+    # strip out the potential leading and trailing quotes
+    file_path = file_path.gsub('"', '')
+
+    # check for and read in file
+    unless File.exist?(file_path)
+      runner.registerError("No file found at file path '#{file_path}'")
+      return false
+    end
+
+    # read in csv and transform to an array of hashes
+    runner.registerInfo("Reading file at file path '#{file_path}'")
+    raw_data =  CSV.table(file_path)
+    variables = raw_data.map { |row| row.to_hash }
+    if variables.empty?
+      runner.registerError("File at file path '#{file_path}' is empty")
+      return false
+    end
+
     # build up meter definition
-    if !File.exist? file_path
-      runner.registerError("The file at path #{file_path} doesn't exist.")
-      return false
-    else
-      raw_data =  CSV.table(file_path)
-      # Transform to array of hashes
-      variables = raw_data.map { |row| row.to_hash }
-      variables.each do |var|
-        key_name = var[:key_name]
-        output_variable_or_meter_name = var[:output_variable_or_meter_name]
-        meter_definition = meter_definition + "," + "#{key_name}" + "," + "#{output_variable_or_meter_name}"
-      end
+    runner.registerInfo("Creating custom meter: '#{custom_meter_name}'")
+    meter_custom = OpenStudio::Model::MeterCustom.new(model)
+    meter_custom.setName(custom_meter_name)
+    meter_custom.setFuelType(fuel_type)
+    variables.each do |var|
+      meter_custom.addKeyVarGroup(var[:key_name], var[:output_variable_or_meter_name])
     end
-    
-    meter_definition = meter_definition + ";"    
-    runner.registerInfo("This is the custom meter definition: #{meter_definition}")
-    
-    num_added = 0
-    idf_object = OpenStudio::IdfObject::load(meter_definition)
-    idf_object = idf_object.get    
-    num_added += add_object(runner, workspace, idf_object)
-    
+
+    # add output meter
     if add_output_meter
-      output_meter_definition = "Output:Meter," + custom_meter_name + "," + reporting_frequency + ";"
-      idf_object = OpenStudio::IdfObject::load(output_meter_definition)
-      idf_object = idf_object.get    
-      num_added += add_object(runner, workspace, idf_object)
-      runner.registerFinalCondition("Added #{num_added} meter:custom and output:meter objects.")
-    else
-      runner.registerFinalCondition("Added #{num_added} custom meter objects.")
-    end   
+      output_meter = OpenStudio::Model::OutputMeter.new(model)
+      output_meter.setName(custom_meter_name)
+      output_meter.setReportingFrequency(reporting_frequency)
+      runner.registerInfo("Adding meter for '#{meter_custom.name}' reporting #{reporting_frequency}")
+    end
+
+    # reporting final condition
+    runner.registerFinalCondition("Added a custom meter object with #{meter_custom.numKeyVarGroups} key-variable groups.")
 
     return true
- 
-  end 
-
-end 
+  end
+end
 
 # register the measure to be used by the application
 MeterCustom.new.registerWithApplication
--- meter_custom/tests/meter_custom_test.rb
+++ meter_custom/tests/meter_custom_test.rb
@@ -1,28 +1,79 @@
 require 'openstudio'
 require 'openstudio/ruleset/ShowRunnerOutput'
 require 'minitest/autorun'
-
 require_relative '../measure.rb'
 
-class MeterCustom_Test < MiniTest::Unit::TestCase
+class MeterCustom_Test < Minitest::Test
 
-  # def setup
-  # end
-
-  # def teardown
-  
   def test_number_of_arguments_and_argument_names
-  
+    puts 'test_number_of_arguments_and_argument_names'
     # create an instance of the measure
     measure = MeterCustom.new
-    
-    # make an empty workspace
-    workspace = OpenStudio::Workspace.new
 
     # get arguments and test that they are what we are expecting
-    arguments = measure.arguments(workspace)
-    assert_equal(1, arguments.size)
-    assert_equal("measures_json", arguments[0].name)
+    arguments = measure.arguments
+    assert_equal(5, arguments.size)
+    assert_equal('custom_meter_name', arguments[0].name)
+    assert_equal('fuel_type', arguments[1].name)
+    assert_equal('file_path', arguments[2].name)
+    assert_equal('add_output_meter', arguments[3].name)
+    assert_equal('reporting_frequency', arguments[4].name)
   end
 
+  def test_example_csv_file
+    puts 'test_example_csv_file'
+    assert(File.exist?("#{File.dirname(__FILE__)}/example_file.csv"))
+
+    # create an instance of the measure
+    measure = MeterCustom.new
+
+    # make an empty model
+    model = OpenStudio::Model::Model.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # get arguments
+    arguments = measure.arguments
+
+    # set file path argument
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+    file_path = arguments[2].clone
+    assert(file_path.setValue("#{File.dirname(__FILE__)}/example_file.csv"))
+    argument_map['file_path'] = file_path
+
+    # run the measure and show the output
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+    assert_equal('Success', result.value.valueName)
+  end
+
+  def test_bad_path
+    puts 'test_bad_path'
+
+    # create an instance of the measure
+    measure = MeterCustom.new
+
+    # make an empty model
+    model = OpenStudio::Model::Model.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # get arguments
+    arguments = measure.arguments
+
+    # set file path argument
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+    file_path = arguments[2].clone
+    assert(file_path.setValue("#{File.dirname(__FILE__)}/does_not_exist.csv"))
+    argument_map['file_path'] = file_path
+
+    # run the measure and show the output
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+    assert_equal('Fail', result.value.valueName)
+  end
 end
