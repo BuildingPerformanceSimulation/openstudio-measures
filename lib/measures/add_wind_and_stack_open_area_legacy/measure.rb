be8142f2b11832904c353006d8911fd5c5d4f8c8
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2023-03-07 09:24:20 -0700

    fix height difference calculation
    Use 1/4 window height (half of neutral pressure level, assumed to be the midpoint of the window).

--- lib/measures/add_wind_and_stack_open_area_legacy/measure.rb
+++ lib/measures/add_wind_and_stack_open_area_legacy/measure.rb
@@ -7,12 +7,12 @@ class AddWindAndStackOpenAreaLegacy < OpenStudio::Measure::EnergyPlusMeasure
 
   # human readable description
   def description
-    return "This measure models natural ventilation to thermal zones with operable windows.  It is not intended to model natural ventilation that relies on interzone, stack driven air transfer."
+    return "This measure models natural ventilation to thermal zones with operable casement type windows.  It is not intended to model natural ventilation that relies on interzone, stack driven air transfer."
   end
 
   # human readable description of modeling approach
   def modeler_description
-    return "This measure adds ZoneVentilation:WindandStackOpenArea objects to a zone for each window of a specified operable window construction.  The user can specify values for minimum and maximum zone and outdoor air temperatures and wind speed that set limits on when the ventilation is active. The airflow rate is the quadrature sum of wind driven and stack effect driven air flow.  Airflow driven by wind is a function of opening effectiveness, area, scheduled open area fraction, and wind speed.  Airflow driven by the stack effect is a function of the discharge coefficient, area, scheduled open area fraction, and height difference to the neutral pressure level.  This measure takes the height difference as half the window height, and as such is only intended to model natural ventilation in single zones where a few large operable windows or doors account for the majority of operable area.  It is not intended to model natural ventilation that relies on interzone, stack driven air transfer where ventilation flow through a opening is unidirectional."
+    return "This measure adds ZoneVentilation:WindandStackOpenArea objects to a zone for each window of a specified operable window construction. The user can specify values for minimum and maximum zone and outdoor air temperatures and wind speed that set limits on when the ventilation is active. The airflow rate is the quadrature sum of wind driven and stack effect driven air flow. Airflow driven by wind is a function of opening effectiveness, area, scheduled open area fraction, and wind speed. Airflow driven by the stack effect is a function of the discharge coefficient, area, scheduled open area fraction, and height difference to the neutral pressure level. This measure takes the height difference as one quarter the window height, and as such is only intended to model natural ventilation in single zones where a few large operable casement type windows or doors account for the majority of operable area. It is not intended to model natural ventilation that relies on interzone, stack driven air transfer where ventilation flow through a opening is unidirectional."
   end
 
   ###
@@ -142,7 +142,7 @@ class AddWindAndStackOpenAreaLegacy < OpenStudio::Measure::EnergyPlusMeasure
     end
 
     height = (zmax - zmin)
-    return height / 2.0
+    return height / 4.0
   end
 
   def get_window_effective_angle(workspace, window)
@@ -498,7 +498,7 @@ class AddWindAndStackOpenAreaLegacy < OpenStudio::Measure::EnergyPlusMeasure
       # calculate open_area size
       open_area = get_window_area(workspace, w)
 
-      # calculate the height_difference at one half the window height.  See notes for explanation.
+      # calculate the height_difference at one quarter the window height. Assuming a casement type window or any type of door with the neutral pressure level (NPL) assumed to be 1/2 the window height and the midpoint of the lower opening (MP) being one half of the height between the bottom of the window and the NPL.
       height_difference = get_window_height_difference(workspace, w)
 
       # determine outward normal angle for surface; need one for each window object
